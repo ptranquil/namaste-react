bfc4f739243987e56db038ba639e8f3df410e276
Prashant Dwivedi <prashant.dwivedi@routemobile.com>
2025-03-24 16:07:13 +0530

    episode-01

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const parent = React.createElement("div", { id: "parent"}, React.createElement("div", { id: "child"}, [
+    React.createElement("h1", {}, "Hello World from React"),
+    React.createElement("h2", {
+        id: 'heading'
+    }, "Hello World from React")    
+]))
+
+// const h1 = React.createElement("h1", {
+//     id: 'heading'
+// }, "Hello World from React");
+
+// const h2 = React.createElement("h2", {}, "hello wordld from Recat")
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(`parent`)
