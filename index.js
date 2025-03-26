723c41724d4f4cc27ac15ddec7d301a5080e57ab
Prashant Dwivedi <prashant.dwivedi@routemobile.com>
2025-03-26 18:42:34 +0530

    episode 03

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent"}, React.createElement("div", { id: "child"}, [
-    React.createElement("h1", {}, "Hello World from React!!!!!"),
-    React.createElement("h2", {
-        id: 'heading'
-    }, "Hello World from React")    
-]))
+// const parent = React.createElement("div", { id: "parent"}, React.createElement("div", { id: "child"}, [
+//     React.createElement("h1", {}, "Hello World from React!!!!!"),
+//     React.createElement("h2", {
+//         id: 'heading'
+//     }, "Hello World from React")    
+// ]))
 
 // const h1 = React.createElement("h1", {
 //     id: 'heading'
@@ -14,6 +14,16 @@ const parent = React.createElement("div", { id: "parent"}, React.createElement("
 
 // const h2 = React.createElement("h2", {}, "hello wordld from Recat")
 
+const Title = () => {
+    return <h3>Hello World from React!!!!!</h3>
+}
+const Heading = () => (
+    <div className="hello">
+        <h1>I am main component</h1>
+        <Title />
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(parent)
+root.render(< Heading />)
